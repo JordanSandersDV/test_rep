2ce1d31ec2f31dfb5dfc725e3ee2471a76767767
vladk <vladk@devart.com>
2025-12-02 15:47:25 +0100

    Closed #1

---
+++ Tables/Production.ProductCategory.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [Production].[ProductCategory] (
+  [ProductCategoryID] [int] IDENTITY,
+  [Name] [dbo].[Name] NOT NULL,
+  [rowguid] [uniqueidentifier] NOT NULL CONSTRAINT [DF_ProductCategory_rowguid] DEFAULT (newid()) ROWGUIDCOL,
+  [ModifiedDate] [datetime] NOT NULL CONSTRAINT [DF_ProductCategory_ModifiedDate] DEFAULT (getdate()),
+  CONSTRAINT [PK_ProductCategory_ProductCategoryID] PRIMARY KEY CLUSTERED ([ProductCategoryID])
+)
+ON [PRIMARY]
+GO
+
+CREATE UNIQUE INDEX [AK_ProductCategory_Name]
+  ON [Production].[ProductCategory] ([Name])
+  ON [PRIMARY]
+GO
+
+CREATE UNIQUE INDEX [AK_ProductCategory_rowguid]
+  ON [Production].[ProductCategory] ([rowguid])
+  ON [PRIMARY]
+GO
