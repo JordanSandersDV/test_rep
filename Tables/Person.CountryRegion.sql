190f47b1912fa2ec859f12708e46fcb8ca4ec43b
vladk <vladk@devart.com>
2025-12-02 15:46:19 +0100

    Fixes #1

---
+++ Tables/Person.CountryRegion.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [Person].[CountryRegion] (
+  [CountryRegionCode] [nvarchar](3) NOT NULL,
+  [Name] [dbo].[Name] NOT NULL,
+  [ModifiedDate] [datetime] NOT NULL CONSTRAINT [DF_CountryRegion_ModifiedDate] DEFAULT (getdate()),
+  CONSTRAINT [PK_CountryRegion_CountryRegionCode] PRIMARY KEY CLUSTERED ([CountryRegionCode])
+)
+ON [PRIMARY]
+GO
+
+CREATE UNIQUE INDEX [AK_CountryRegion_Name]
+  ON [Person].[CountryRegion] ([Name])
+  ON [PRIMARY]
+GO
